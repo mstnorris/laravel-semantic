7d6a04e6e21b11990d79e33d04e1cc87774677af
Michael Norris <mike@michaelnorris.co.uk>
2015-04-15 14:53:23 +0100

    semantic ui inclusion

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,17 @@
 var elixir = require('laravel-elixir');
 
 elixir(function(mix) {
-    mix.sass('app.scss')
-        .copy(
-        'vendor/bower_components/bootstrap-sass-official/assets/stylesheets/',
-        'resources/assets/sass/bootstrap'
+    mix.copy(
+        'vendor/bower_components/semantic-ui/dist/semantic.css',
+        'public/css/vendor/semantic-ui/semantic.css'
     )
         .copy(
-        'vendor/bower_components/font-awesome-sass/assets/stylesheets/',
-        'resources/assets/sass/font-awesome'
+        'vendor/bower_components/semantic-ui/dist/semantic.js',
+        'public/js/vendor/semantic-ui/semantic.js'
     )
         .copy(
-        'vendor/bower_components/font-awesome-sass/assets/fonts/',
-        'public/fonts'
+        'vendor/bower_components/jquery/dist/jquery.js',
+        'public/js/vendor/jquery/jquery.js'
     )
-        .copy(
-        'vendor/bower_components/animate-sass/',
-        'resources/assets/sass/animate'
-    )
-        .copy(
-        'vendor/bower_components/jquery/dist/jquery.min.js',
-        'public/js/jquery.js'
-    )
-        .copy(
-        'vendor/bower_components/jquery/dist/jquery.min.map',
-        'public/js/jquery.min.map'
-    )
-        .copy(
-        'vendor/bower_components/bootstrap-sass-official/assets/javascripts/bootstrap.js',
-        'public/js/bootstrap.js'
-    );
+        .stylesIn("public/css");
 });
